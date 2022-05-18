2365d1be8c773d7639ef54bc1d3765aa93c6089a
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-18 13:50:55 +0300

    new config

--- config/project.py
+++ config/project.py
@@ -1,4 +1,1 @@
-project_name = "demos-haskell"
-project_long_description = "Demos for the haskell language"
-project_year_started = 2014
-project_description = """Demos for the haskell language"""
+description_short = "Demos for the haskell language"
